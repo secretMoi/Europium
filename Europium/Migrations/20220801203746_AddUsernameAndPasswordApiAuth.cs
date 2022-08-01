ecb42b933d9580c42df9d0131555d8565ae057ae
secretMoi <win.moi@hotmail.com>
2022-08-01 23:02:59 +0200

    Add username and password for api

---
+++ Europium/Migrations/20220801203746_AddUsernameAndPasswordApiAuth.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Europium.Migrations
+{
+    public partial class AddUsernameAndPasswordApiAuth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "ApisToMonitor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "ApisToMonitor",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "ApisToMonitor");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "ApisToMonitor");
+        }
+    }
+}
