a203295f6de2f7cfe64bc0acbc84546df54c0592
Lampirou <win.moi@hotmail.com>
2023-06-28 23:23:37 +0200

    Remove https redirection

--- Europium/Program.cs
+++ Europium/Program.cs
@@ -120,7 +120,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 var configProgram = app.Services.GetService<ConfigProgram>()!;
 app.UseStaticFiles(new StaticFileOptions
